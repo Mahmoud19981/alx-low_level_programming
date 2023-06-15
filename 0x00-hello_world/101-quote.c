c674a5192d826b1e2c7b1d9402b37318aa45f7e0
Mahmoud19981 <mahmoud5555654@gmail.com>
2023-06-15 12:25:46 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 /**
  * main - Entry point
  * Description: Print a qoute useing write function
- * Return: 1 (Not success)
+ *	ssize_t write(int fd,const void *buf,size_t count);
+ * Return: 1 (error).
 */
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1.quo.59);
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
