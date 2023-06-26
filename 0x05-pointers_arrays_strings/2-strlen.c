28a73fbabf288a6394bae8c10668edb2fbc0c8bd
Mahmoud19981 <mahmoud5555654@gmail.com>
2023-06-26 11:11:05 -0700

    m

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,19 @@
 #include "main.h"
+
 /**
- * _strlen - return the length of string
+ * _strlen - returns the length of a string
+ *
  * @s: string parameter input
- * return: length of string
+ *
+ * Return: length of string
 */
+
 int _strlen(char *s)
 {
 	int count;
+
 	for (count = 0; *s != '\0'; ++s)
 		++count;
+
 	return (count);
 }
