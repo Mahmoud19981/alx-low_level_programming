1a05a687a998bfcf6e3aadc0c1f9dc4065281c87
Mahmoud19981 <mahmoud5555654@gmail.com>
2023-07-03 12:20:06 -0700

    m

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,8 +7,8 @@
  *
  * @s: in the string
  *
- * return: a pointer to the first occurrence of the character
-*/
+ * Return: a pointer to the first occurrence of the character
+ */
 
 char *_strchr(char *s, char c)
 {
